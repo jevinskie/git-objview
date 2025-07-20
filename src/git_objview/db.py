cf4da295819a394a3300df789434a2c678a30412
Jevin Sweval <jevinsweval@gmail.com>
2025-07-19 21:22:33 -0400

    utils

--- src/git_objview/db.py
+++ src/git_objview/db.py
@@ -5,3 +5,7 @@ from path import Path
 @define(auto_attribs=True)
 class RepoDB:
     path: Path = field(converter=Path)
+
+    @property
+    def git_obj_dir(self) -> Path:
+        return self.path / ".git" / "objects"
