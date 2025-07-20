74b2a339b3c923e412ffeae3cd52442113d03354
Jevin Sweval <jevinsweval@gmail.com>
2025-07-20 00:59:46 -0400

    cool

--- src/git_objview/app.py
+++ src/git_objview/app.py
@@ -1,6 +1,7 @@
 from path import Path
 from textual import events
 from textual.app import App, ComposeResult
+from textual.containers import Vertical
 from textual.widgets import (
     Header,
     Placeholder,
@@ -18,26 +19,27 @@ class GitObjViewApp(App):
 
     _last_esc_time: float | None
 
-    def __init__(self, *args, git_repo_path: Path, **kwargs) -> None:
+    def __init__(self, *args, git_repo_path: Path | None = None, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self._git_repo_path = git_repo_path
+        self._git_repo_path = git_repo_path if git_repo_path is not None else Path()
         self._last_esc_time = None
 
     def compose(self) -> ComposeResult:
-        yield Header(show_clock=True, id="hdr")
-        with TabbedContent(id="main"):
-            with TabPane("References", id="refs"):
-                yield Placeholder("refs go here")
-            with TabPane("Objects", id="objs"):
-                yield Placeholder("objs go here")
-            with TabPane("Commits", id="cmts"):
-                yield Placeholder("cmts go here")
-            with TabPane("Trees", id="tres"):
-                yield Placeholder("tres go here")
-            with TabPane("Blobs", id="blbs"):
-                yield Placeholder("blbs go here")
-            with TabPane("Tags", id="tags"):
-                yield Placeholder("tags go here")
+        with Vertical(id="main"):
+            yield Header(show_clock=True, id="hdr")
+            with TabbedContent(id="browser"):
+                with TabPane("References", id="refs"):
+                    yield Placeholder("refs go here")
+                with TabPane("Objects", id="objs"):
+                    yield Placeholder("objs go here")
+                with TabPane("Commits", id="cmts"):
+                    yield Placeholder("cmts go here")
+                with TabPane("Trees", id="tres"):
+                    yield Placeholder("tres go here")
+                with TabPane("Blobs", id="blbs"):
+                    yield Placeholder("blbs go here")
+                with TabPane("Tags", id="tags"):
+                    yield Placeholder("tags go here")
         yield RichLog(id="log")
 
     def do_exit(self, message: str = "<> HAVE <> A <> GREAT <> DAY <>") -> None:
@@ -57,8 +59,3 @@ class GitObjViewApp(App):
 
     def on_ready(self) -> None:
         pass
-
-
-class GitObjViewAppCwd(GitObjViewApp):
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, git_repo_path=Path(), **kwargs)
